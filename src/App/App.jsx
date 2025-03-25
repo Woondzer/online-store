eb7912a7113b94b382f6d85ea20022d37c4016ce
Elias <elias@st-innovations.net>
2025-03-25 20:49:13 +0100

    added firebase config

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,8 +3,11 @@ import RouteSwitch from "../components/RouteSwitch";
 import Navbar from "../components/Navigation/Navbar";
 import InfoTop from "../components/InfoTop";
 import Footer from "../components/Footer";
+import { auth, db } from "../firebaseConfig";
 
 function App() {
+  console.log("Firebase auth:", auth);
+  console.log("Firebase Firestore DB:", db);
   return (
     <div className="flex flex-col min-h-screen">
       <InfoTop />
